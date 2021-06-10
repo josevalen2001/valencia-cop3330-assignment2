80992b03551af34c81ac8d58861bae293ebe555d
josevalen2001 <jose.juda.valencia@gmail.com>
2021-06-09 23:04:21 -0400

    Commit 2

---
+++ src/main/java/ex25/app.java
@@ -0,0 +1,14 @@
+package ex25;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Jose Valencia
+ */
+
+public class app {
+
+    public static void main(String[] args) {
+
+    }
+
+}
